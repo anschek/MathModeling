4fc6cf3fe6016106fc80f24a45de496a4f3f2134
Alexandra Schekaleva <anschek@yandex.ru>
2024-02-10 22:13:56 +0300

    Clear

--- MathMod/TransportProblem.cs
+++ MathMod/TransportProblem.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
-using System.Runtime;
-using System.Reflection.Metadata;
-using System.Xml;
-
-namespace MathMod
+﻿namespace MathMod
 {
     internal class TransportProblem
     {
         List<int> a_;
         List<int> b_;
-        //тарифы и целевая функция
         List<List<int>> rates_;
         List<List<int>> optimal_;
-
         ClosednessType type_;
 
         enum ClosednessType
@@ -41,7 +29,7 @@ namespace MathMod
 
         static int ListSum(List<int> list)
         {
-            return list.Select(x => x).Sum(); ;
+            return list.Select(x => x).Sum();
         }
         //свести задачу к закрытой
         void ReduceProblemToClosed()
@@ -72,12 +60,11 @@ namespace MathMod
             for (int i = 0; i < matrix.Count; ++i)
             {
                 for (int j = 0; j < matrix[0].Count; ++j)
-                {
                     Console.Write(matrix[i][j] + "  ");
-                }
                 Console.WriteLine();
             }
         }
+
         static int GetMaxElement(List<List<int>> cur_rates)
         {
             return cur_rates.Select(x => x.Max()).Max();
@@ -92,25 +79,17 @@ namespace MathMod
         {
             int min_element = GetMinElement(cur_rates);
             for (int i = 0; i < cur_rates.Count; ++i)
-            {
                 for (int j = 0; j < cur_rates[0].Count; ++j)
-                {
                     if (cur_rates[i][j] == min_element)
-                    {
                         return (i, j);
-                    }
-                }
-            }
             return (-1, -1);//в случае ошибки
         }
 
-        static List<int> Init1DList( int size, int fill)
+        static List<int> Init1DList(int size, int fill)
         {
             List<int> list = new List<int>();
-            for(int i = 0;i < size; ++i)
-            {
+            for (int i = 0; i < size; ++i)
                 list.Add(fill);
-            }
             return list;
         }
 
@@ -118,19 +97,15 @@ namespace MathMod
         {
             List<int> copy = new List<int>();
             for (int i = 0; i < orig.Count; ++i)
-            {
                 copy.Add(orig[i]);
-            }
             return copy;
         }
 
         static List<List<int>> Init2DList(int size0, int size1, int fill)
         {
-            List < List<int> >list = new List<List<int>>();
+            List<List<int>> list = new List<List<int>>();
             for (int i = 0; i < size0; ++i)
-            {
                 list.Add(Init1DList(size1, fill));
-            }
             return list;
         }
 
@@ -138,16 +113,14 @@ namespace MathMod
         {
             List<List<int>> copy = new List<List<int>>();
             for (int i = 0; i < orig.Count; ++i)
-            {
                 copy.Add(Init1DList(orig[i]));
-            }
             return copy;
         }
 
         public void MethodOfMinElement()
         {
             //инициализация списков
-            List<List<int>> cur_rates = Init2DList(rates_), 
+            List<List<int>> cur_rates = Init2DList(rates_),
                             cur_func = Init2DList(rates_.Count, rates_[0].Count, 0);
             List<int> cur_a = Init1DList(a_), cur_b = Init1DList(b_);
             //алгоритм
@@ -163,22 +136,21 @@ namespace MathMod
             optimal_ = cur_func;
         }
 
-        static void CalculatePenalties(List<List<int>> cur_rates,  ref List<int> column_of_penalty, ref List<int> row_of_penalty)
+        static void CalculatePenalties(List<List<int>> cur_rates, ref List<int> column_of_penalty, ref List<int> row_of_penalty)
         {
             int max_elem_to_add = GetMaxElement(cur_rates);
             int min = max_elem_to_add, premin = max_elem_to_add;
             //штрафы пос строкам
-            for(int i = 0;i < cur_rates.Count; ++i)
+            for (int i = 0; i < cur_rates.Count; ++i)
             {
-                for(int j = 0;j < cur_rates[0].Count; ++j)
+                for (int j = 0; j < cur_rates[0].Count; ++j)
                 {
                     if (cur_rates[i][j] < min)
                     {
                         premin = min;
                         min = cur_rates[i][j];
                     }//разность должна рассчитываться между разными элементами
-                    else if (cur_rates[i][j] < premin && cur_rates[i][j] !=min) premin = cur_rates[i][j];
-
+                    else if (cur_rates[i][j] < premin && cur_rates[i][j] != min) premin = cur_rates[i][j];
                 }
                 //если значения явл-ся исходными, строка уже закрыта, если есть только один минимум, он и является штрафом
                 //если минимума два - штраф явл-ся их разностью
@@ -188,7 +160,7 @@ namespace MathMod
                 min = max_elem_to_add; premin = max_elem_to_add;
             }
             //штрафы по столбцам
-            for (int j = 0;j < cur_rates[0].Count; ++j)
+            for (int j = 0; j < cur_rates[0].Count; ++j)
             {
                 min = max_elem_to_add; premin = max_elem_to_add;
                 for (int i = 0; i < cur_rates.Count; ++i)
@@ -206,7 +178,7 @@ namespace MathMod
             }
         }
         //Индексы минимального элемента с максимальным штрафом
-        static (int,int) IndexesOfMinElementWithMaxPenalty(List<List<int>> cur_rates, List<int> column_of_penalty, List<int> row_of_penalty)
+        static (int, int) IndexesOfMinElementWithMaxPenalty(List<List<int>> cur_rates, List<int> column_of_penalty, List<int> row_of_penalty)
         {
             //если макс элемент находится в столбце штрафов
             if (column_of_penalty.Max() > row_of_penalty.Max())
@@ -223,7 +195,7 @@ namespace MathMod
                 int min_element_in_column = cur_rates[0][ind_max_in_row];
                 int min_ind = 0;
                 for (int i = 1; i < cur_rates.Count; ++i)
-                    if (cur_rates[i][ind_max_in_row] < min_element_in_column) 
+                    if (cur_rates[i][ind_max_in_row] < min_element_in_column)
                     {
                         min_element_in_column = cur_rates[i][ind_max_in_row];
                         min_ind = i;
@@ -239,32 +211,32 @@ namespace MathMod
                             cur_func = Init2DList(rates_.Count, rates_[0].Count, 0);
             List<int> cur_a = Init1DList(a_), cur_b = Init1DList(b_);
             //штрафы
-            List<int> row_of_penalty = Init1DList(b_.Count,0), 
-                      column_of_penalty = Init1DList(a_.Count,0);
+            List<int> row_of_penalty = Init1DList(b_.Count, 0),
+                      column_of_penalty = Init1DList(a_.Count, 0);
             //найти макс элемент матрицы, которым будут заменяться ячейки, чтобы их нельзя было определить, как минимальные
             int max_element_to_add = GetMaxElement(cur_rates);
-            while(cur_a.Sum() > 0 && cur_b.Sum() > 0)
+            while (cur_a.Sum() > 0 && cur_b.Sum() > 0)
             {//расчет штрафов, поиск индексов нужного эл-та
                 CalculatePenalties(cur_rates, ref column_of_penalty, ref row_of_penalty);
-                (int i, int j)= IndexesOfMinElementWithMaxPenalty(cur_rates, column_of_penalty, row_of_penalty);
+                (int i, int j) = IndexesOfMinElementWithMaxPenalty(cur_rates, column_of_penalty, row_of_penalty);
                 int supply = Math.Min(cur_a[i], cur_b[j]);
                 //если в ряде минимум, в текущих тарифах заменяем элемент, чтоб к нему не возвращаться
-                if(cur_a[i] == supply)for (int jk = 0; jk < cur_rates.Count; jk++)cur_rates[i][jk] = max_element_to_add;
-                if(cur_b[j] == supply)for (int ik = 0; ik < cur_rates.Count; ik++) cur_rates[ik][j] = max_element_to_add;
+                if (cur_a[i] == supply) for (int jk = 0; jk < cur_rates.Count; jk++) cur_rates[i][jk] = max_element_to_add;
+                if (cur_b[j] == supply) for (int ik = 0; ik < cur_rates.Count; ik++) cur_rates[ik][j] = max_element_to_add;
                 cur_rates[i][j] = max_element_to_add;
                 cur_func[i][j] = supply;
                 cur_a[i] -= supply;
                 cur_b[j] -= supply;
             }
             optimal_ = cur_func;
-        }        
+        }
         public void NorthWestAngleMethod()
         {
             //инициализация списков
             List<List<int>> cur_func = Init2DList(rates_.Count, rates_[0].Count, 0);
             List<int> cur_a = Init1DList(a_), cur_b = Init1DList(b_);
             //проходимя по ячейкам, сдивагая их по степени того, как заканчиваются грузы/потребности
-            int i=0, j=0;
+            int i = 0, j = 0;
             while (cur_a.Sum() > 0 && cur_b.Sum() > 0)
             {
                 int cur_i = i, cur_j = j;
@@ -281,20 +253,21 @@ namespace MathMod
         (Dictionary<(int, int), int>, Dictionary<(int, int), int>) FindPreferredElements(List<List<int>> cur_rates)
         {
             List<List<int>> preferred = Init2DList(rates_.Count, rates_[0].Count, 0);
+            //выбираем минимумы строк
             for (int i = 0; i < cur_rates.Count; ++i)
             {
                 int cur_min = cur_rates[i][0];
                 int min_ind = 0;
                 for (int j = 1; j < cur_rates[0].Count; ++j)
                 {
-                    if (cur_rates[i][j] < cur_min) 
-                    { 
+                    if (cur_rates[i][j] < cur_min)
+                    {
                         cur_min = cur_rates[i][j];
                         min_ind = j;
                     }
                 }
                 ++preferred[i][min_ind];
-            }
+            }//выбираем минимумы столбцов
             for (int j = 0; j < cur_rates[0].Count; ++j)
             {
                 int cur_min = cur_rates[0][j];
@@ -308,40 +281,40 @@ namespace MathMod
                     }
                 }
                 ++preferred[min_ind][j];
-            }
-            Dictionary<(int, int), int> preferred_plus_plus= new Dictionary<(int, int), int>();
+            }//добавляем словари для сортировки по элементам
+            Dictionary<(int, int), int> preferred_plus_plus = new Dictionary<(int, int), int>();
             Dictionary<(int, int), int> preferred_plus = new Dictionary<(int, int), int>();
-            for(int i = 0; i < preferred.Count; ++i)
+            for (int i = 0; i < preferred.Count; ++i)
             {
-                for(int j=0; j < preferred[0].Count; ++j)
+                for (int j = 0; j < preferred[0].Count; ++j)
                 {
-                    if (preferred[i][j] == 2) preferred_plus_plus[(i,j)] = cur_rates[i][j]; 
-                    else if (preferred[i][j] ==1)preferred_plus_plus[(i, j)] = cur_rates[i][j]; 
+                    if (preferred[i][j] == 2) preferred_plus_plus[(i, j)] = cur_rates[i][j];
+                    else if (preferred[i][j] == 1) preferred_plus_plus[(i, j)] = cur_rates[i][j];
                 }
             }
-            return (preferred_plus_plus.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value), 
+            return (preferred_plus_plus.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value),
                 preferred_plus.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value));
         }
-        
+
         public void DoublePreferenceMethod()
         {
             //инициализация списков
             List<List<int>> cur_rates = Init2DList(rates_),
                             cur_func = Init2DList(rates_.Count, rates_[0].Count, 0);
             List<int> cur_a = Init1DList(a_), cur_b = Init1DList(b_);
-            //метод
+            //найти макс элемент матрицы, которым будут заменяться ячейки, чтобы их нельзя было определить, как минимальные
             int max_element_to_add = GetMaxElement(cur_rates);
             (Dictionary<(int, int), int> plus_plus, Dictionary<(int, int), int> plus) = FindPreferredElements(cur_rates);
             foreach (((int i, int j), _) in plus_plus)
-            {
+            {//проходимся по ++
                 int supply = Math.Min(cur_a[i], cur_b[j]);
                 cur_rates[i][j] = max_element_to_add;
                 cur_func[i][j] = supply;
                 cur_a[i] -= supply;
                 cur_b[j] -= supply;
-            }            
+            }
             foreach (((int i, int j), _) in plus)
-            {
+            {//проходимся по +
                 int supply = Math.Min(cur_a[i], cur_b[j]);
                 cur_rates[i][j] = max_element_to_add;
                 cur_func[i][j] = supply;
@@ -349,7 +322,7 @@ namespace MathMod
                 cur_b[j] -= supply;
             }
             while (cur_a.Sum() > 0 && cur_b.Sum() > 0)
-            {
+            {//из остальных эл-тов выбираем минимальные
                 (int i, int j) = GetMinIndex(cur_rates);
                 int supply = Math.Min(cur_a[i], cur_b[j]);
                 cur_rates[i][j] = GetMaxElement(cur_rates);
@@ -359,7 +332,7 @@ namespace MathMod
             }
             optimal_ = cur_func;
         }
-       
+
         public List<List<int>> GetOptimalPlan()
         {
             return optimal_;
@@ -368,7 +341,7 @@ namespace MathMod
         public bool IsNonDegenerate()
         {
             return (a_.Count + b_.Count - 1
-                == optimal_.SelectMany(x => x).Count(x => x!=0));
+                == optimal_.SelectMany(x => x).Count(x => x != 0));
         }
         //получить целевую функцию
         public int GetObjectiveFunction()
@@ -378,13 +351,9 @@ namespace MathMod
             int columns = rates_[0].Count;
             if (type_ == ClosednessType.ShortageOfSupply) --rows;
             if (type_ == ClosednessType.SurplusOfSupply) --columns;
-            for (int i = 0;i <rows;  ++i)
-            {
+            for (int i = 0; i < rows; ++i)
                 for (int j = 0; j < columns; ++j)
-                {
-                   objective_func += rates_[i][j] * optimal_[i][j];
-                }
-            }
+                    objective_func += rates_[i][j] * optimal_[i][j];
             return objective_func;
         }
     }
